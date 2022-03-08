06c7341882c12803caca3bb91820c36ed95a94b6
Marat <zinatulinmarat@gmail.com>
2022-03-08 12:34:28 +0500

    add routing, guard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { WebsiteModule } from './routing/website/website.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([]),
+    WebsiteModule
   ],
-  providers: [],
+  providers: [
+    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/routing/website/routing/home/home.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { HomePagesComponent } from './pages/home-pages/home-pages.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomePagesComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {path: '', pathMatch: 'full', component: HomePagesComponent}
+    ])
+  ]
+})
+export class HomeModule { }

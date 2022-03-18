2fea1d5a1b23ae5ec8f9bbaf106996b8fc93ea4c
Marat <zinatulinmarat@gmail.com>
2022-03-18 12:45:29 +0500

    work with main page

--- src/app/routing/website/routing/admin/admin.module.ts
+++ src/app/routing/website/routing/admin/admin.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import {AdminPageComponent} from "./pages/admin-page/admin-page.component";
+import {AdminFooterBlockModule} from "../../../../view/admin-footer-block/admin-footer-block.module";
+import {AdminHeaderBlockModule} from "../../../../view/admin-header-block/admin-header-block.module";
 
 
 
 
 @NgModule({
   declarations: [
-    
+    AdminPageComponent
   ],
   imports: [
     CommonModule,
@@ -19,10 +22,13 @@ import { RouterModule } from '@angular/router';
       },
       {
         path: 'dashboard',
+        component: AdminPageComponent,
         loadChildren: () => import('./routing/dashboard/dashboard.module')
           .then(module => module.DashboardModule)
       }
-    ])
+    ]),
+    AdminFooterBlockModule,
+    AdminHeaderBlockModule
   ]
 })
 export class AdminModule { }
---
+++ src/app/view/admin-footer-block/blocks/admin-footer-block/admin-footer-block.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-footer-block',
+  templateUrl: './admin-footer-block.component.html',
+  styleUrls: ['./admin-footer-block.component.scss']
+})
+export class AdminFooterBlockComponent {
+
+  date = new Date();
+
+}
